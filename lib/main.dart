42a56ef9d9c307fcb7d7855fee18ffbddbcb5655
Williano <paawilly17@gmail.com>
2020-04-16 00:29:00 -0500

    Add package setting

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:instagram_ui/routes/router.dart';
 import 'package:instagram_ui/utilities/route_constants.dart';
+//import 'package:device_preview/device_preview.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,10 +11,18 @@ void main() {
       .then((_) => runApp(InstagramUI()));
 }
 
+// void main() => runApp(
+//       DevicePreview(
+//         builder: (context) => InstagramUI(),
+//       ),
+//     );
+
 class InstagramUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // locale: DevicePreview.of(context).locale, // <--- Add the locale
+      // builder: DevicePreview.appBuilder, // <--- Add the builder
       debugShowCheckedModeBanner: false,
       onGenerateRoute: generateRoute,
       initialRoute: StartScreenRoute,
